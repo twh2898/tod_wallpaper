8f729fb932c6f9da6dda5618ace90c63e847b6d8
Thomas <twh2898@vt.edu>
2016-07-21 01:25:11 -0400

    Fixed time index conversion around 1:00 AM

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::process::Command;
 use std::time::Duration;
+// use std::io::prelude::*;
+// use std::fs::File;
 use std::thread;
 use std::fs;
 
@@ -19,22 +21,62 @@ fn get_files(path: &str) -> Vec<String> {
     return vector;
 }
 
+struct Config{
+    path: String,
+    files: Vec<String>,
+    update: u64
+}
+
+/*  Broken  */
+// fn get_config() -> Result<Config, String> {
+//     let mut f = File::open("config").unwrap_or_else(|e| Err(String::from("Error loading file!")));
+//     let mut s = String::new();
+//     f.read_to_string(&mut s).expect("Could not read file");
+//     println!("Read {} lines", s.len());
+//     let path: String = String::from("/usr/share/tod_wallpaper/");
+//     // let files = get_files(&path);
+//     let files: Vec<String> = vec![];
+//     let update = 60;
+//
+//     // let mut iter = s.split(|c| c == '\n').filter(|line| line.chars().next() != b'#');
+//     for line in s.split('\n') {
+//         if line.chars().next().unwrap() != '#' || line.split_whitespace().count() < 1{
+//             continue;
+//         }
+//
+//         for obj in line.split_whitespace() {
+//             println!("{}", obj);
+//         }
+//         println!("");
+//     }
+//
+//     Ok(Config{
+//         path: path,
+//         files: files,
+//         update: update
+//     });
+// }
+
 fn main() {
-    let path = "/usr/share/tod_wallpaper/";
-    let files = get_files(path);
-    let update = 60;
+    // let conf = get_config().unwrap();
+    let conf = Config {
+        path: String::from("/usr/share/tod_wallpaper/"),
+        files: get_files("/usr/share/tod_wallpaper/"),
+        update: 60
+    };
     let mut last_img = -1;
 
-    println!("Loaded {} files with delay {} from {}", files.len(), update, path);
+    println!("Loaded {} files with delay {} from {}", conf.files.len(), conf.update, conf.path);
 
     loop {
         let now = time::now();          // get current time
-        let i = (now.tm_hour / 2) - 1;  // go to next image every two hours
+        let i = now.tm_hour / 2;        // go to next image every two hours
 
+        println!("At {:?} load image {}", now, i);
         // check if the image needs to be updated
         if i != last_img {
             last_img = i;
-            let v = &files[i as usize]; // get the image path
+            let v = &conf.files[i as usize]; // get the image path
 
             println!("Time of day {}:{} showing {}", now.tm_hour, now.tm_min, v);
 
@@ -45,6 +87,6 @@ fn main() {
                     .expect("failed to execute process");
         }
 
-        thread::sleep(Duration::new(update, 0));    // sleep for some time before next check for update
+        thread::sleep(Duration::new(conf.update, 0));    // sleep for some time before next check for update
     }
 }
