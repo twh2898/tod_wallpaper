ae52336f812d7798292555f775932eb4c1bac4c4
Thomas Harrison <twh2898@vt.edu>
2017-12-14 23:33:30 -0500

    Added clap, added build / install scripts, re-wrote code, updated readme, added image files

--- README.md
+++ README.md
@@ -2,6 +2,37 @@
 
 This program will update your wallpaper based on the time of day. Currently, every **2** hours, the wallpaper will switch to the next of **12** wallpapers in the directory `/usr/shared/tod_wallpapers/`
 
+## Usage
+
+### Install
+
+```
+$ git clone https://github.com/twh2898/tod_wallpaper.git
+$ cd tod_wallpaper/
+$ ./build
+$ sudo ./install
+```
+
+### Use
+
+```
+$ tod_wallpaper
+```
+
+## Dependencies
+
+`feh` is used to set the wallpaper on an Xorg desktop
+
+## Config
+
+Loaded from
+* `~/.config/tod_wallpaper/config`
+* `~/.tod_wallpaper/config`
+* `/etc/tod_wallpaper/config`
+
+* picture directory
+* hour seperation
+
 ## Todo
 
 - [ ] Load image folder path from config file
--- src/main.rs
+++ src/main.rs
@@ -1,91 +1,101 @@
-use std::process::Command;
-use std::time::Duration;
-// use std::io::prelude::*;
-// use std::fs::File;
-use std::thread;
-use std::fs;
 
 extern crate time;
 
-fn get_files(path: &str) -> Vec<String> {
-    let mut paths = fs::read_dir(path)
-                        .unwrap_or_else(|e| panic!("Could not read directory {}\n{}", path, e));
-    let mut vector: Vec<String> = Vec::new();
+#[macro_use]
+extern crate clap;
+use clap::App;
 
-    while let Some(Ok(p)) = paths.next() {
-        let s = p.path().to_str().unwrap().to_string();
-        vector.push(s);
+use std::fs;
+use std::path::Path;
+use std::time::Duration;
+use std::thread;
+
+const SEC_PER_MIN: u64 = 60;
+const MINUETS_PER_DAY: i32 = 1440;
+const DEFAULT_DIRECTORY: &str = "/usr/share/tod_wallpaper/";
+
+/// Calculate the curent image index and the delay in seconds to the next image.
+fn calculate_deltas(delay: i32) -> (usize, u64) {
+    let now = time::now();
+    let minuets_today = now.tm_hour * 60 + now.tm_min;
+    let image_i = minuets_today / delay;
+    let delay_next = delay - (minuets_today % delay);
+
+    (image_i as usize, delay_next as u64)
+}
+
+/// Load image list from directory and calculate delay if not provided.
+fn load<P: AsRef<Path>>(directory: P, delay: Option<i32>) -> (Vec<fs::DirEntry>, i32) {
+    let directory = directory.as_ref();
+
+    // Check if directory exists
+    if !directory.exists() {
+        println!("Image directory does not exist {:?}", directory);
+        std::process::exit(-1);
     }
 
-    vector.sort();
-    return vector;
+    // Load image list
+    let mut images = fs::read_dir(directory)
+        .unwrap_or_else(|e| {
+            println!("Failed to read image directory {:?}\n{}", directory, e);
+            std::process::exit(-1);
+        })
+        .filter_map(|f| f.ok())
+        .collect::<Vec<_>>();
+
+    // Check that there are files
+    if images.len() < 1 {
+        println!("No images present in {:?}", directory);
+        std::process::exit(-1);
+    }
+
+    // Sort files
+    images.sort_by(|a, b| a.path().cmp(&b.path()));
+
+    // Calculate delay if not set
+    // Delay is in minuets
+    let delay: i32 = delay.unwrap_or(MINUETS_PER_DAY / images.len() as i32);
+
+    (images, delay)
 }
 
-struct Config{
-    path: String,
-    files: Vec<String>,
-    update: u64
-}
-
-/*  Broken  */
-// fn get_config() -> Result<Config, String> {
-//     let mut f = File::open("config").unwrap_or_else(|e| Err(String::from("Error loading file!")));
-//     let mut s = String::new();
-//     f.read_to_string(&mut s).expect("Could not read file");
-//     println!("Read {} lines", s.len());
-//     let path: String = String::from("/usr/share/tod_wallpaper/");
-//     // let files = get_files(&path);
-//     let files: Vec<String> = vec![];
-//     let update = 60;
-//
-//     // let mut iter = s.split(|c| c == '\n').filter(|line| line.chars().next() != b'#');
-//     for line in s.split('\n') {
-//         if line.chars().next().unwrap() != '#' || line.split_whitespace().count() < 1{
-//             continue;
-//         }
-//
-//         for obj in line.split_whitespace() {
-//             println!("{}", obj);
-//         }
-//         println!("");
-//     }
-//
-//     Ok(Config{
-//         path: path,
-//         files: files,
-//         update: update
-//     });
-// }
-
+/// Application's entry point
 fn main() {
-    // let conf = get_config().unwrap();
-    let conf = Config {
-        path: String::from("/usr/share/tod_wallpaper/"),
-        files: get_files("/usr/share/tod_wallpaper/"),
-        update: 60
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
+
+    // Get arguments
+    let directory = Path::new(matches.value_of("directory").unwrap_or(DEFAULT_DIRECTORY));
+    let delay = match matches.value_of("delay") {
+        Some(delay) => delay.parse::<i32>().ok(),
+        None => None,
     };
-    let mut last_img = -1;
 
-    println!("Loaded {} files with delay {} from {}", conf.files.len(), conf.update, conf.path);
+    let (images, delay) = load(directory, delay);
 
+    // Main loop
     loop {
-        let now = time::now();          // get current time
-        let i = now.tm_hour / 2;        // go to next image every two hours
+        let (image_i, next_delay) = calculate_deltas(delay);
 
-        // check if the image needs to be updated
-        if i != last_img {
-            last_img = i;
-            let v = &conf.files[i as usize]; // get the image path
+        // Bounds checking
+        let image_i = if image_i > images.len() {
+            images.len() - 1
+        } else {
+            image_i
+        };
 
-            println!("Time of day {}:{} showing {}", now.tm_hour, now.tm_min, v);
+        // Get the next image
+        let next_image = &images[image_i];
 
-            Command::new("feh")         // run feh with the image path
-                    .arg("--bg-fill")   // > feh --bg-fill [path]
-                    .arg(v)
-                    .status()
-                    .expect("failed to execute process");
-        }
+        // Run feh with the next image's path
+        // $ feh --bg-fill <path>
+        std::process::Command::new("feh")
+            .arg("--bg-fill")
+            .arg(next_image.path())
+            .status()
+            .expect("failed to execute process: feh");
 
-        thread::sleep(Duration::new(conf.update, 0));    // sleep for some time before next check for update
+        // Sleep until the next update is due
+        thread::sleep(Duration::from_secs(next_delay * SEC_PER_MIN));
     }
 }
